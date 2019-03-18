f465f32b08930bb690cb85a7c7d1b8be5855f6ba
Hannah Coffman <hcoffman7@gmail.com>
2019-03-18 23:49:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const drivers =  ["Milo", "Otis", "Garfield"]
 
 function destructivelyPrependDriver(name) {
         drivers.slice('Bob')
+        drivers[]
 }
 
 function destructivelyRemoveLastDriver(name) {
