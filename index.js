63846d443c9bcf0237e0d6b4bf6ded947988628c
Hannah Coffman <hcoffman7@gmail.com>
2019-03-18 23:56:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,13 +19,13 @@ function destructivelyRemoveFirstDriver(name) {
 
 
 function appendDriver(name) {
-     let driversCopy = drivers.slice()
-     driversCopy.push(name)
+     let driverscopy = drivers.slice()
+     driverscopy.push(name)
      return driverscopy
 }
 
 function prependDriver(name) {
-     let driversCopy2 = drivers.slice()
-     driversCopy2.unshift(name)
+     let driverscopy2 = drivers.slice()
+     driverscopy2.unshift(name)
      return driverscopy2
 }
