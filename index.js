1c6cf2813e073c997c9a7128ad1a0f10980a6c49
Hannah Coffman <hcoffman7@gmail.com>
2019-03-18 23:26:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,4 @@
 const drivers =  ["Milo", "Otis", "Garfield"]
 
 destructivelyAppendDriver(name)
-drivers.push 
- describe('destructivelyAppendDriver(name)'
+drivers.push('destructivelyAppendDriver(name)')
